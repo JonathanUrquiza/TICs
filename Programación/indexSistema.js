ac2e459a8d782d0484b75c75953ed2ca2ef34a99
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-05-27 22:54:31 -0300

    correcciones

--- Programación/indexSistema.js
+++ Programación/indexSistema.js
@@ -174,3 +174,27 @@ p.innerHTML = fecha.toLocaleDateString();
 //Modificando el DOM
 dia.appendChild(p);
 
+/* const menuLinks = document.querySelectorAll('.navbar__ul a[href^="#"]');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const menuLink = document.querySelector(`.navbar__ul a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        document.querySelector(".navbar__ul a.seleccionado").classList.remove("seleccionado");
+        menuLink.classList.add("seleccionado");
+      }
+    });
+  },
+  { rootMargin: "-30% 0px -70% 0px" }
+);
+
+menuLinks.forEach((menuLink) => {
+  const hash = menuLink.getAttribute("href");
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+}); */
